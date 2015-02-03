0a2cfefe8d6bd90ec749c414d5e777b64b521c9a
Reilly Watson <reillywatson@gmail.com>
2015-02-03 02:02:08 -0500

    add title

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.get("/", function(req, res) {
-	res.send("<html><head><title>PARTY TIME!!!!</title><body><img src=\"lindi.jpg\"/></body></html>");
+	res.send("<html><head><title>PARTY TIME!!!!</title><body><h1>PARTY TIME!!!</h1><br/><img src=\"lindi.jpg\"/></body></html>");
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
