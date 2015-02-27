20be8e815c1dc4bbb8c7ef71ad84a52b54c013bb
Reilly Watson <reillywatson@gmail.com>
2015-02-26 23:35:45 -0500

    Use static

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ var path = require("path");
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.get("/", function(req, res) {
-	res.send("<html><head><title>WEDDING!!!</title><body><h1>PARTY TIME!!!</h1><img src=\"lindi.jpg\"/><img src=\"reilly.jpg\"/></body></html>");
-});
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
 	console.log("Listening on " + port);
